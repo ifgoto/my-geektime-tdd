ece09aaffceb21df8b352c2a31d999497d3f5602
jun <jun.peng1@tec-do.com>
2022-07-16 23:22:51 +0800

    Refactor: 消除重复-使用泛型

--- 02.args/src/main/java/geektime/tdd/args/SingleValueOptionParser.java
+++ 02.args/src/main/java/geektime/tdd/args/SingleValueOptionParser.java
@@ -3,16 +3,16 @@ package geektime.tdd.args;
 import java.util.List;
 import java.util.function.Function;
 
-class SingleValueOptionParser implements OptionParser {
+class SingleValueOptionParser<T> implements OptionParser {
 
-    Function<String, Object> valueParser;
+    Function<String, T> valueParser;
 
-    public SingleValueOptionParser(Function<String, Object> valueParser) {
+    public SingleValueOptionParser(Function<String, T> valueParser) {
         this.valueParser = valueParser;
     }
 
     @Override
-    public Object parse(List<String> arguments, Option option) {
+    public T parse(List<String> arguments, Option option) {
         int index = arguments.indexOf("-" + option.value());
         String value = arguments.get(index + 1);
         return valueParser.apply(value);

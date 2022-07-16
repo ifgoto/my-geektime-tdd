7efda9787cef640b49557da2d5ecba15831bb9f4
jun <jun.peng1@tec-do.com>
2022-07-16 22:55:42 +0800

    Refactor: 消除重复-移动Function优化

--- 02.args/src/main/java/geektime/tdd/args/IntOptionParser.java
+++ 02.args/src/main/java/geektime/tdd/args/IntOptionParser.java
@@ -5,7 +5,7 @@ import java.util.function.Function;
 
 class IntOptionParser implements OptionParser {
 
-    Function<String, Integer> valueParser = value1 -> Integer.valueOf(value1);
+    Function<String, Integer> valueParser = Integer::parseInt;
 
     @Override
     public Object parse(List<String> arguments, Option option) {

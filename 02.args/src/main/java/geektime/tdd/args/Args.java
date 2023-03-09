5eeabceb23bcb4ff9bf68485337152c4221aed7b
wtg <ifgoto@sina.com>
2023-03-10 01:26:49 +0800

    before MultiOpition passed.

--- 02.args/src/main/java/geektime/tdd/args/Args.java
+++ 02.args/src/main/java/geektime/tdd/args/Args.java
@@ -1,34 +1,42 @@
 package geektime.tdd.args;
 
+
+import java.lang.annotation.Annotation;
 import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Parameter;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Map;
 
 public class Args {
-
     public static <T> T parse(Class<T> optionsClass, String... args) {
-
         try {
-            List<String> arguments = Arrays.asList(args);
             Constructor<?> constructor = optionsClass.getDeclaredConstructors()[0];
+            Parameter parameter = constructor.getParameters()[0];
+            Option option = parameter.getAnnotation(Option.class);
+            List<String> argument = Arrays.asList(args);
 
-            Object[] values = Arrays.stream(constructor.getParameters()).map(it -> parseOption(arguments, it)).toArray();
 
-            return (T) constructor.newInstance(values);
+            Object value = null;
+            if (parameter.getType() == boolean.class){
+
+                value = argument.contains("-" + option.value());
+            }
+
+            if (parameter.getType() == int.class) {
+                int index = argument.indexOf("-" + option.value());
+                value = Integer.parseInt(argument.get(index + 1));
+            }
+
+            if (parameter.getType() == String.class) {
+                int index = argument.indexOf("-" + option.value());
+                value = argument.get(index+1);
+            }
+
+            return (T) constructor.newInstance(value);
+
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
     }
-
-    private static Object parseOption(List<String> arguments, Parameter parameter) {
-        return PARSERS.get(parameter.getType()).parse(arguments, parameter.getAnnotation(Option.class));
-    }
-
-    private static Map<Class<?>, OptionParser> PARSERS = Map.of(
-            boolean.class, new BooleanParser(),
-            int.class, new SingleValueOptionParser<>(Integer::parseInt),
-            String.class, new SingleValueOptionParser<>(String::valueOf));
-
 }
--- 02.args/src/test/java/geektime/tdd/args/ArgsTest.java
+++ 02.args/src/test/java/geektime/tdd/args/ArgsTest.java
@@ -1,6 +1,5 @@
 package geektime.tdd.args;
 
-import com.sun.jdi.connect.Connector;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
@@ -18,11 +17,73 @@ public class ArgsTest {
 
 
     @Test
-    public void should_set_optino_to_true_if_flag_present() {
-        BooleanOption option = Args.parse(BooleanOption.class, "-l");
-        assertTrue(option.logging());
+    public void should_parse_multi_options() {
+        MultiOptions options = Args.parse(MultiOptions.class, "-l", "-p", "8080",
+                "-d", "/usr/logs");
+
+        assertTrue(options.logging());
+        assertEquals(8080, options.port());
+        assertEquals("/usr/logs", options.directory());
+
     }
-    static record BooleanOption(@Option("l")boolean logging){}
+    static record MultiOptions(@Option("l") boolean logging, @Option("p") int port,
+                               @Option("d") String directory) {}
+
+   @Test
+   public void should_set_boolean_option_to_true_if_flag_present() {
+        BooleanOption option = Args.parse(BooleanOption.class, "-l");
+        assertTrue(option.looging());
+    }
+
+    @Test
+    public void should_set_boolean_option_to_false_if_flag_present() {
+        BooleanOption option = Args.parse(BooleanOption.class);
+        assertFalse(option.looging());
+    }
+
+    static record BooleanOption(@Option("l")boolean looging){}
+
+
+    //Integer -p 8080
+
+    @Test
+    public void should_parse_int_as_option_value() {
+        IntOption option = Args.parse(IntOption.class, "-p", "8080");
+        assertEquals(8080, option.port());
+    }
+
+    static record IntOption(@Option("p")int port) {}
+
+    // TODO: String -d /usr/logs
+    @Test
+    public void should_parse_stringF_as_option_value() {
+        StringOption option = Args.parse(StringOption.class,  "-d", "/usr/logs");
+        assertEquals("/usr/logs", option.directory());
+    }
+
+    static record StringOption(@Option("d")String directory) {}
+
+//    @Test
+//    @Disabled
+//    public void should() {
+//        Options optioin = Args.parse("l:b,p:d,d:s", "-l", "-p", "8080");
+//        args.getBool("l");
+//        args.getInt("p");
+//
+//        Options options = args.parse(Options.class, "-l", "-p", "8880", "-d", "/usr/logs");
+//
+//    }
+//
+//    static record Options(@Option("l") boolean loggin, @Option("p") int port, @Option("d") String directory) {}
+
+//    @Test
+//    public void should_set_optino_to_true_if_flag_present() {
+//        BooleanOption option = Args.parse(BooleanOption.class, "-l");
+//        assertTrue(option.logging());
+//    }
+//    static record BooleanOption(@Option("l")boolean logging){}
+
+
 
 //    @Test
 //    @Disabled

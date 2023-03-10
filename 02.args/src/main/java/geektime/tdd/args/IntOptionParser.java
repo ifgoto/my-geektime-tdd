9b1059b010eaf726debbcd8892f8ed891d797ed0
wtg <ifgoto@sina.com>
2023-03-10 19:45:07 +0800

    通过 extends 去除代码重复

--- 02.args/src/main/java/geektime/tdd/args/IntOptionParser.java
+++ 02.args/src/main/java/geektime/tdd/args/IntOptionParser.java
@@ -7,6 +7,11 @@ class IntOptionParser implements OptionParser {
     @Override
     public Object parse(List<String> arguments, Option option) {
         int index = arguments.indexOf("-" + option.value());
-        return Integer.parseInt(arguments.get(index + 1));
+        String value = arguments.get(index + 1);
+        return parseValue(value);
+    }
+
+    protected Object parseValue(String value) {
+        return Integer.parseInt(value);
     }
 }
--- 02.args/src/main/java/geektime/tdd/args/StringOptionParse.java
+++ 02.args/src/main/java/geektime/tdd/args/StringOptionParse.java
@@ -2,11 +2,11 @@ package geektime.tdd.args;
 
 import java.util.List;
 
-class StringOptionParse implements OptionParser{
+class StringOptionParse extends IntOptionParser{
 
     @Override
-    public Object parse(List<String> arguments, Option option) {
-        int index = arguments.indexOf("-" + option.value());
-        return arguments.get(index+1);
+
+    protected Object parseValue(String value) {
+        return String.valueOf(value);
     }
 }
